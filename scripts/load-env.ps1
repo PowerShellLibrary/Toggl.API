2cf815e3296709238285586cc19fbb720c8abf68
Alan Płócieniak <alan-null@outlook.com>
2025-02-03 23:04:44 +0100

    Do not expose .env content

--- scripts/load-env.ps1
+++ scripts/load-env.ps1
@@ -1,8 +1,18 @@
+param (
+    [switch]$Verbose
+)
+
 Get-Content "$PSScriptRoot\..\.env" | ForEach-Object {
     if ($_ -match '^(.*?)=(.*)$') {
         $key = $matches[1].Trim()
         $value = $matches[2].Trim()
-        Write-Host "Setting environment variable: $key=$value" -ForegroundColor Green
+
+        if ($Verbose) {
+            Write-Host "Setting environment variable: $Key=$value" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Setting environment variable: $Key=********" -ForegroundColor Green
+        }
         Set-Item "env:$key" $value
     }
 }
